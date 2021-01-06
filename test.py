01ad20a682b185e79d6857772d7dff9a85cb4f71
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-01-06 09:35:17 +0100

    auto launch

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ fps = FPS().start()
 while True:
 
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = imutils.resize(frame, width=1080)
 	print(frame.shape) 
 	(h, w) = frame.shape[:2]
 	resized_image = cv2.resize(frame, (300, 300))
